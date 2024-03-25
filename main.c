36140d7e3e4a591efff2b63f8ae2502568e4fa64
ZakHarris25 <zakharris25@gmail.com>
2024-03-25 19:27:29 +0000

    Created mazegen and environment

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+/*TODO
+ * read csv to ints
+ * store value of csv in array of structs
+ * using array of structs find possible path (MANUALLY)
+ * create A* using manhattan pathing EUCLID CAN SMD
+ * functions needed:
+ * Read and parse csv
+ * a*
+ *
+ *
+ * */
+
+
+struct Tile{
+    int east;
+    int 
+};
+
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
---
+++ mazeGen.py
@@ -0,0 +1,4 @@
+from pyamaze import maze
+m=maze(6,8)
+m.CreateMaze(saveMaze=True)
+m.run()
