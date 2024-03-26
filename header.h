4b8c6528feeba7da9c213b6f06b2530b7febdb27
ZakHarris25 <zakharris25@gmail.com>
2024-03-26 23:53:09 +0000

    removed csv intake, manually filled table, algorithm next

--- header.h
+++ header.h
@@ -11,6 +11,6 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-
-char csvExtract(int c);
+struct Move aStar();
--- main.c
+++ main.c
@@ -6,13 +6,23 @@
  * using array of structs find possible path (MANUALLY)
  * create A* using manhattan pathing EUCLID CAN SMD
  * functions needed:
- * Read and parse csv
+ * reading csv can fuck itself{
+ *          Read and parse csv
+ * }
  * a*
  *
  *
  * */
 
-
+//STARTING tile [5][0]
+/*
+ * east true
+ * west OUTSIDE WALL
+ * north FALSE
+ * south OUTSIDE WALL
+ * opening facing up
+ *
+ */
 struct Tile {
     char east;
     char west;
@@ -20,153 +30,28 @@ struct Tile {
     char north;
 };
 
+struct Move{
+    bool right;
+    bool left;
+    bool down;
+    bool up;
+};
+
 int main() {
     struct Tile tiles[6][8]={
-            {{},{},{},{},{},{},{},{}},
-            {{},{},{},{},{},{},{},{}},
-            {{},{},{},{},{},{},{},{}},
-            {{},{},{},{},{},{},{},{}},
-            {{},{},{},{},{},{},{},{}},
-            {{},{},{},{},{},{},{},{}}
+            {{.east = 0, .west = 0, .south =0, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1}},
+            {{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 0},{.east = 0, .west = 0, .south =1, .north= 1}},
+            {{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =0, .north= 0},{.east = 0, .west = 0, .south =1, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =1, .north= 1}},
+            {{.east = 0, .west = 0, .south =1, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 0, .west = 0, .south =1, .north= 1}},
+            {{.east = 1, .west = 0, .south =1, .north= 1},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 0, .west = 0, .south =1, .north= 1}},
+            {{.east = 1, .west = 0, .south =1, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0}}
     };
-    csvExtract(1);
-    for (int l = 0; l < 4; ++l) {
-        switch (l) {
-            case 0:
-                for (int i = 0; i < 6; ++i)
-                {
-                    for (int j = 0; j < 8; ++j)
-                    {
-                        tiles[i][j].east = csvExtract(l);
-                        printf("EAST at %i,%i: %c\n ", i, j, tiles[i][j].east);
-                    }
-                }
-                break;
-            case 1:
-                for (int i = 0; i < 5; ++i)
-                {
-                    for (int j = 0; j < 7; ++j)
-                    {
-                        tiles[i][j].west = csvExtract(l);
-                        printf("WEST at %i,%i: %c\n",i, j, tiles[i][j].west);
-                    }
-                }
-                break;
-            case 2:
-                for (int i = 0; i < 5; ++i)
-                {
-                    for (int j = 0; j < 7; ++j)
-                    {
-                        tiles[i][j].south = csvExtract(l);
-                        printf("SOUTH: %c\n", tiles[i][j].south);
-                    }
-                }
-                break;
-            case 3:
-                for (int i = 0; i < 5; ++i)
-                {
-                    for (int j = 0; j < 7; ++j)
-                    {
-                        tiles[i][j].north = csvExtract(l);
-                        printf("NORTH: %c\n", tiles[i][j].north);
-                    }
-                }
-                break;
-            default:
-                perror("Main Switch error");
 
-        }
-    }
+
     return 0;
 }
 
 
+ struct Move aStar(){
 
-char csvExtract(int c){
-    char fileLine[16];
-    const char check[5] = ",()\"";
-    char *token;
-
-    FILE *fp;
-    //opening file and giving error if unavailable
-    fp = fopen("C:\\Users\\Zak\\Documents\\GitHub\\MazeSolvingFinal\\mazeOne.csv", "r");
-    if(fp == NULL)
-    {
-        perror("Error opening file");
-        return(-1);
-    }
-
-    //checking if fgets results in NULL - returning error
-    if(fgets(fileLine, 24, fp) == NULL)
-    {
-        perror("ERROR");
-        return(-1);
-    }else
-    {
-        puts(fileLine);
-    }
-
-    while( token != NULL ) {
-
-        token = strtok(NULL, check);
-
-        switch (c) {
-            case 0:
-                for (int i = 0; i < 6; ++i)
-                {
-                    if(i==2)
-                    {
-                        return(token[i]);
-                    }else
-                        token = strtok(NULL, check);
-                }
-                break;
-            case 1:
-                for (int i = 0; i < 6; ++i)
-                {
-                    if(i==3)
-                    {
-                        return(token[i]);
-                    }else
-                        token = strtok(NULL, check);
-                }
-                break;
-            case 2:
-                for (int i = 0; i < 6; ++i)
-                {
-                    if(i==4)
-                    {
-                        return(token[i]);
-                    }else
-                        token = strtok(NULL, check);
-                }
-                break;
-            case 3:
-                for (int i = 0; i < 6; ++i)
-                {
-                    if(i==5)
-                    {
-                        return(token[i]);
-                    }else
-                        token = strtok(NULL, check);
-                }
-                break;
-            default:
-                perror("switch error: ");
-                printf("c = %i", c);
-                break;
-
-        }
-    }
-    for (int i = 0; i < 5; ++i)
-    {
-        if(i==2)
-        {
-            return(token[i]);
-        }else {
-            token = strtok(NULL, check);
-        }
-    }
-    return(-1);
 };
-
