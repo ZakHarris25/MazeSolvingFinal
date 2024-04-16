aeac708cfe09d82ed223dbc959109d0c39495d97
ZakHarris25 <zakharris25@gmail.com>
2024-04-16 13:42:12 +0100

    Added move functionality. creates movelist. DOES NOT HANDLE MULTIPLE OPTIONS WELL

--- header.h
+++ header.h
@@ -1,43 +1,63 @@
+////
+//// Created by Zak on 26/03/2024.
+////
+
+
+
+#include "stdio.h"
+#include "stdbool.h"
+
+#define ROWS 8
+#define COLS 10
+
+int startEndCO[4];
+int movelist[64][2] = {0};
+int currentPos[2];
+int moveOptions[4];
+
+int lastMove;
+int c = 0;
+
+void findSE();
+void startUp();
+int validMove();
+void changePos();
+//#ifndef MAZESOLVINGFINAL_HEADER_H
+//#define MAZESOLVINGFINAL_HEADER_H
 //
-// Created by Zak on 26/03/2024.
+//#endif //MAZESOLVINGFINAL_HEADER_H
 //
-
-#ifndef MAZESOLVINGFINAL_HEADER_H
-#define MAZESOLVINGFINAL_HEADER_H
-
-#endif //MAZESOLVINGFINAL_HEADER_H
-
-
-
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
-int start[2] = {1, 1};
-int end[2] = {6,8};
-
-int *gValues;
-int *hValues;
-
-struct Tile {
-    int pos[2];
-    char east;
-    char west;
-    char south;
-    char north;
-
-    int f;
-    int g;
-    int h;
-};
-
-int* gCalc(int x, int y);
-int* hCalc(int x, int y);
-
-
-struct Move{
-    bool right;
-    bool left;
-    bool down;
-    bool up;
-};
+//
+//
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdbool.h>
+//
+//int start[2] = {1, 1};
+//int end[2] = {6,8};
+//
+//int *gValues;
+//int *hValues;
+//
+//struct Tile {
+//    int pos[2];
+//    char east;
+//    char west;
+//    char south;
+//    char north;
+//
+//    int f;
+//    int g;
+//    int h;
+//};
+//
+//int* gCalc(int x, int y);
+//int* hCalc(int x, int y);
+//
+//
+//struct Move{
+//    bool right;
+//    bool left;
+//    bool down;
+//    bool up;
+//};
--- main.c
+++ main.c
@@ -18,49 +18,138 @@
  *
  * */
 
-//STARTING tile [5][0]
-
-/*
- * east true
- * west OUTSIDE WALL
- * north FALSE
- * south OUTSIDE WALL
- * opening facing up
- *
- */
-
-int main() {
-    int openList[48];
-    int closedList[48];
-
-    struct Tile tiles[6][8]={
-            {{.east = 0, .west = 0, .south =0, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1}},
-            {{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 0},{.east = 0, .west = 0, .south =1, .north= 1}},
-            {{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =0, .north= 0},{.east = 0, .west = 0, .south =1, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =1, .north= 1}},
-            {{.east = 0, .west = 0, .south =1, .north= 1},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 0, .west = 0, .south =1, .north= 1}},
-            {{.east = 1, .west = 0, .south =1, .north= 1},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 1},{.east = 0, .west = 1, .south =0, .north= 1},{.east = 0, .west = 0, .south =1, .north= 1}},
-            {{.east = 1, .west = 0, .south =1, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =0, .north= 0},{.east = 1, .west = 1, .south =0, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0},{.east = 1, .west = 0, .south =1, .north= 0},{.east = 0, .west = 1, .south =1, .north= 0}}
-    };
+// Define maze elements
+#define WALL '#'
+#define PASSAGE ' '
+#define START 'S'
+#define EXIT 'E'
 
 
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < 8; ++j) {
 
 
-        }
+
+int maze1[ROWS][COLS] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 2, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
+        {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+        {1, 1, 0, 1, 1, 1, 0, 1, 0, 1},
+        {1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 0, 0, 0, 0, 1, 0, 1, 3, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+
+
+int main(){
+    findSE();
+    startUp();
+    for (int i = 0; i < 15; ++i) {
+        changePos();
+        ++c;
     }
-    return  0;
+
+
+//    for (int i = 0; i < 4; ++i) {//prints coordinated of start and end points
+//        if(i == 2){
+//            printf("\n");
+//        }
+//        printf("%i, ", startEndCO[i]);
+//    }
+    return 0;
 }
 
-int *gCalc(int x, int y){
-    *gValues = start[0] - x;
-    if (start[0] > x){
-        *gValues = start[0] - x;
-        printf("t");
-        return(gValues);
-    }else{
-        *gValues = x - start[0];
-        printf("t");
-        return(gValues);
+
+
+
+void findSE(){//function to find the start and end points of the maze
+    for (int i = 0; i < 7; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            switch (maze1[i][j]){
+                case 2:
+                    startEndCO[0] = i;
+                    startEndCO[1] = j;
+                    break;
+                case 3:
+                    startEndCO[2] = i;
+                    startEndCO[3] = j;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+}
+
+void startUp(){
+    currentPos[0] = startEndCO[0];
+    currentPos[1] = startEndCO[1];
+    printf("Current POSITION: %i,%i\n", currentPos[0], currentPos[1]);
+    printf("start POSITION: %i , %i\n", startEndCO[0], startEndCO[1]);
+    printf("end POSITION: %i,%i\n", startEndCO[2], startEndCO[3]);
+
+}
+
+
+int validMove(){//checks possible valid moves returns 1-4 based on direction of available move
+    for (int i = 1; i < 5; ++i) {
+        switch (i) {
+            case 1:
+                if(maze1[currentPos[0]][currentPos[1]+1]  != 1 && lastMove != 2){
+                    printf("\nRight is valid: ");
+                    lastMove = 1;
+                    moveOptions[0] = 1;
+                }
+                break;
+            case 2:
+                if(maze1[currentPos[0]][currentPos[1]-1]  != 1 && lastMove != 1){
+                    printf("\nLeft is valid: ");
+                    lastMove = 2;
+                    moveOptions[1] = 1;
+                }
+                break;
+            case 3:
+                if(maze1[currentPos[0]-1][currentPos[1]]  != 1 && lastMove !=4){
+                    printf("\nUp is valid: ");
+                    lastMove = 3;
+                    moveOptions[2] = 1;
+                }
+                break;
+                case 4:
+                    if(maze1[currentPos[0]+1][currentPos[1]]  != 1 && lastMove != 3){
+                    printf("\nDown is valid: ");
+                    lastMove = 4;
+                    moveOptions[3] = 1;
+                }
+                break;
+            default:
+                perror("Switch Failure in ValidMove function");
+                return 0;
+                break;
+        }
+    }
+    return 0;
+}
+
+void changePos(){
+    for (int i = 0; i < 4; ++i) {
+        if(moveOptions[0] == 1){
+            currentPos[1] += 1;
+            movelist[c][0] = currentPos[0];
+            movelist[c][1] = currentPos[1];
+        }else if(moveOptions[1] == 1){
+            currentPos[1] += 1;
+            movelist[c][0] = currentPos[0];
+            movelist[c][1] = currentPos[1];
+        }else if(moveOptions[2] == 1){
+            currentPos[0] -= 1;
+            movelist[c][0] = currentPos[0];
+            movelist[c][1] = currentPos[1];
+        }else{
+            currentPos[0] += 1;
+            movelist[c][0] = currentPos[0];
+            movelist[c][1] = currentPos[1];
+        }
+    }
+        printf("%i, %i", currentPos[0], currentPos[1]);
 }
