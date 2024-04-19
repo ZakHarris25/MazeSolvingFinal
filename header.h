4eacb6296bef2428a8b7b03d351009c32bf90f6e
ZakHarris25 <zakharris25@gmail.com>
2024-04-19 14:38:49 +0100

    Finished first maze. Needs commenting more and tidying. EDGE case testing pos

--- header.h
+++ header.h
@@ -2,62 +2,27 @@
 //// Created by Zak on 26/03/2024.
 ////
 
+#ifndef MAZESOLVINGFINAL_HEADER_H
+#define MAZESOLVINGFINAL_HEADER_H
 
+#endif //MAZESOLVINGFINAL_HEADER_H
 
 #include "stdio.h"
 #include "stdbool.h"
-
-#define ROWS 8
-#define COLS 10
-
+int maze1[6][8] = { // maze initialisation 1 represents walls, 2 represents the start point, 3 represents the end and 0 represents open passages
+        {2, 1, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0, 1, 1, 1},
+        {0, 0, 0, 1, 0, 0, 0, 0},
+        {1, 0, 1, 1, 1, 0, 1, 0},
+        {1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 1, 0, 1, 3},
+};
 int startEndCO[4];
-int movelist[64][2] = {0};
 int currentPos[2];
 int moveOptions[4];
-
-int lastMove;
-int c = 0;
-
+int lastMove = -2;
+bool winCon();
 void findSE();
 void startUp();
-int validMove();
-void changePos();
-//#ifndef MAZESOLVINGFINAL_HEADER_H
-//#define MAZESOLVINGFINAL_HEADER_H
-//
-//#endif //MAZESOLVINGFINAL_HEADER_H
-//
-//
-//
-//#include <stdio.h>
-//#include <string.h>
-//#include <stdbool.h>
-//
-//int start[2] = {1, 1};
-//int end[2] = {6,8};
-//
-//int *gValues;
-//int *hValues;
-//
-//struct Tile {
-//    int pos[2];
-//    char east;
-//    char west;
-//    char south;
-//    char north;
-//
-//    int f;
-//    int g;
-//    int h;
-//};
-//
-//int* gCalc(int x, int y);
-//int* hCalc(int x, int y);
-//
-//
-//struct Move{
-//    bool right;
-//    bool left;
-//    bool down;
-//    bool up;
-//};
+void checkDist();
+void bestMove();
--- main.c
+++ main.c
@@ -1,73 +1,19 @@
 #include "header.h"
 
-/*TODO
- * read csv to ints                                     /
- * store value of csv in array of structs               /
- * using array of structs find possible path (MANUALLY)
- * create A* using manhattan pathing EUCLID CAN SMD
- * functions needed:
- * reading csv can fuck itself{                         /
- *          Read and parse csv
- * }
- * a*
- * create two queues/lists
- * open list is set of nodes that need to be evaluated
- * closed is a set of nodes that have already been evaluated
- * add start node to open
- *
- *
- * */
-
-// Define maze elements
-#define WALL '#'
-#define PASSAGE ' '
-#define START 'S'
-#define EXIT 'E'
-
-
-
-
-
-int maze1[ROWS][COLS] = {
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 2, 1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
-        {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-        {1, 1, 0, 1, 1, 1, 0, 1, 0, 1},
-        {1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {1, 0, 0, 0, 0, 1, 0, 1, 3, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
-
-
-
 int main(){
     findSE();
     startUp();
-    for (int i = 0; i < 15; ++i) {
-        changePos();
-        ++c;
+    while(winCon() == false){
+        checkDist();
     }
-
-
-//    for (int i = 0; i < 4; ++i) {//prints coordinated of start and end points
-//        if(i == 2){
-//            printf("\n");
-//        }
-//        printf("%i, ", startEndCO[i]);
-//    }
-    return 0;
 }
 
-
-
-
 void findSE(){//function to find the start and end points of the maze
-    for (int i = 0; i < 7; ++i) {
-        for (int j = 0; j < 9; ++j) {
+    for (int i = 0; i < 6; ++i) {               //iterates through and saves the start and end points in one array
+        for (int j = 0; j < 8; ++j) {
             switch (maze1[i][j]){
                 case 2:
-                    startEndCO[0] = i;
+                    startEndCO[0] = i;          //the coordinates are accessed based off (ROWS, COLUMNS) as apposed to x,y to avoid confusion
                     startEndCO[1] = j;
                     break;
                 case 3:
@@ -81,75 +27,98 @@ void findSE(){//function to find the start and end points of the maze
     }
 }
 
-void startUp(){
+void startUp(){ // gives output to the user and sets the current position
     currentPos[0] = startEndCO[0];
     currentPos[1] = startEndCO[1];
-    printf("Current POSITION: %i,%i\n", currentPos[0], currentPos[1]);
-    printf("start POSITION: %i , %i\n", startEndCO[0], startEndCO[1]);
-    printf("end POSITION: %i,%i\n", startEndCO[2], startEndCO[3]);
+    printf("Current POSITION: %i, %i\n", currentPos[0], currentPos[1]);
+    printf("Start POSITION: %i, %i\n", startEndCO[0], startEndCO[1]);
+    printf("End POSITION: %i, %i\n", startEndCO[2], startEndCO[3]);
 
 }
 
-
-int validMove(){//checks possible valid moves returns 1-4 based on direction of available move
-    for (int i = 1; i < 5; ++i) {
-        switch (i) {
-            case 1:
-                if(maze1[currentPos[0]][currentPos[1]+1]  != 1 && lastMove != 2){
-                    printf("\nRight is valid: ");
-                    lastMove = 1;
-                    moveOptions[0] = 1;
-                }
-                break;
-            case 2:
-                if(maze1[currentPos[0]][currentPos[1]-1]  != 1 && lastMove != 1){
-                    printf("\nLeft is valid: ");
-                    lastMove = 2;
-                    moveOptions[1] = 1;
-                }
-                break;
-            case 3:
-                if(maze1[currentPos[0]-1][currentPos[1]]  != 1 && lastMove !=4){
-                    printf("\nUp is valid: ");
-                    lastMove = 3;
-                    moveOptions[2] = 1;
-                }
-                break;
-                case 4:
-                    if(maze1[currentPos[0]+1][currentPos[1]]  != 1 && lastMove != 3){
-                    printf("\nDown is valid: ");
-                    lastMove = 4;
-                    moveOptions[3] = 1;
-                }
-                break;
-            default:
-                perror("Switch Failure in ValidMove function");
-                return 0;
-                break;
-        }
+void checkDist() { // function to find the direction the "mouse" can travel for the longest distance
+    for (int i = 0; i < 4; ++i) {//clears the moveOptions array - was having problems with using both global and local variables so this was a fool proof way of clearing values
+        moveOptions[i] = 0;
     }
-    return 0;
-}
-
-void changePos(){
-    for (int i = 0; i < 4; ++i) {
-        if(moveOptions[0] == 1){
-            currentPos[1] += 1;
-            movelist[c][0] = currentPos[0];
-            movelist[c][1] = currentPos[1];
-        }else if(moveOptions[1] == 1){
-            currentPos[1] += 1;
-            movelist[c][0] = currentPos[0];
-            movelist[c][1] = currentPos[1];
-        }else if(moveOptions[2] == 1){
-            currentPos[0] -= 1;
-            movelist[c][0] = currentPos[0];
-            movelist[c][1] = currentPos[1];
+    for (int i = 1; i < 6; ++i) {//checks whether there is a wall, whether it will be out of bounds and if the last move was the opposite to avoid being trapped in LR or UD loop
+        if(maze1[currentPos[0]+i][currentPos[1]] != 1 && currentPos[0]+i < 5 && lastMove != 1){
+          moveOptions[0]++;//DOWN
         }else{
-            currentPos[0] += 1;
-            movelist[c][0] = currentPos[0];
-            movelist[c][1] = currentPos[1];
+            break;
         }
     }
-        printf("%i, %i", currentPos[0], currentPos[1]);
+    for (int i = 0; i < 6; ++i) {
+        if(maze1[currentPos[0]-i][currentPos[1]] != 1 && currentPos[0]-i > 0 && lastMove != 0){
+            moveOptions[1]++;//UP
+        }else{
+            break;
+        }
+    }
+    for (int i = 1; i < 8; ++i) {
+        if(maze1[currentPos[0]][currentPos[1]+i] != 1 && currentPos[1]+i <8 && lastMove != 3){
+            moveOptions[2]++;//RIGHT
+        }
+        else{
+            break;
+        }
+    }
+    for (int i = 0; i < 8; ++i) {
+        if(maze1[currentPos[0]][currentPos[1]-i] != 1 && currentPos[1]-i > 0 && lastMove != 2){
+            moveOptions[3]++;//LEFT
+        }
+        else{
+            break;
+        }
+    }
+    bestMove();
+}
+
+void bestMove(){
+    int largest = -1;
+    for (int i = 0; i < 4; i++) {
+        if (moveOptions[i] > largest) { // finds the largest of the 4 directions available
+            largest = i;
+        }
+    }
+    switch (largest) { //switch according to the direction that can be followed the furthest. Moves "Mouse" accordingly
+        case 0:
+            printf("\nDOWN\n");
+            currentPos[0] += moveOptions[0];//DOWN
+            lastMove = 0;
+            printf("\nCURRENT POSITION: %i, %i\n", currentPos[0], currentPos[1]);
+
+            break;
+        case 1:
+            printf("\nUP\n");
+            currentPos[0] -= moveOptions[1];//UP
+            lastMove = 1;
+            printf("\nCURRENT POSITION: %i, %i\n", currentPos[0], currentPos[1]);
+            break;
+        case 2:
+            printf("\nRIGHT\n");
+            currentPos[1] += moveOptions[2];//RIGHT
+            lastMove = 2;
+
+            printf("\nCURRENT POSITION: %i, %i\n", currentPos[0], currentPos[1]);
+            break;
+        case 3:
+            printf("\nLEFT\n");
+            currentPos[1] -= moveOptions[3];//LEFT
+            lastMove = 3;
+
+            printf("\nCURRENT POSITION: %i, %i\n", currentPos[0], currentPos[1]);
+            break;
+        default:
+            perror("\nSHITS FUCKED MOVEDIRECTION SWITCH\n");
+            break;
+    }
+};
+
+bool winCon(){//checks whether the mouse is 1 off the end tile to signal that it has finished the maze
+    if(maze1[currentPos[0]-1][currentPos[1]] == 3 || maze1[currentPos[0]+1][currentPos[1]] == 3 || maze1[currentPos[0]][currentPos[1]+1] == 3 || maze1[currentPos[0]+1][currentPos[1]-1] == 3){
+        printf("\nMaze completed");
+        return true;
+    }else{
+        return false;
+    }
 }
